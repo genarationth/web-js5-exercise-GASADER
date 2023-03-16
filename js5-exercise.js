ecb99d179aa839d65dfc1a4c20bce3886060d373
GASADER <kajorhkaen@gmail.com>
2023-03-16 09:54:03 +0700

    complete assessment

---
+++ js5-exercise.js
@@ -0,0 +1,59 @@
+// Exercise #1
+// Part 1
+// let consumer = ['Sofia', 'David', 'Juan']
+// consumer.push('Sara', 'Augustin')
+// consumer.shift()
+// consumer.splice(0, 1, 'Renata')
+// consumer.push('Elena')
+// console.log(consumer)
+
+// Exercise #2
+// let i = 0
+// for (let i = 1; i <= 5; i++) {
+
+//     let result = ''
+
+//     for(let j = 1; j <= i; j++) {
+//         result += '*'
+//     }
+//     console.log(result)
+//   }
+
+// Exercise #3
+// 3.1
+// let i 
+// xValue = (n) => {
+//   for ( i = n; i >= 0; i-=.5) {
+//   console.log(`xValue: ${i} `) 
+// }
+// }
+// xValue(8)
+
+// 3.2
+// let i
+//  oddNumber= (n) => {
+//     for (i = 0; i <= n; (i % 2) !== 0, i++ )
+//     console.log(`oddNumber: ${n} `) 
+//  }
+//  oddNumber(100)
+
+// 3.3
+// let i 
+// printSquare = (n) => {
+//   for ( i = 0; i <= n; i++) {
+//   console.log(`[${i}]`) 
+//   }
+// }
+// printSquare(8)
+
+// 3.4
+//  sumNumber = (n) => {
+//     let sum = 0;
+//     let i = 1;
+//     while (i <= n) {
+//       sum += i;
+//       i++;
+//     }
+//     return sum;
+//   }
+//   console.log(sumNumber(60))
